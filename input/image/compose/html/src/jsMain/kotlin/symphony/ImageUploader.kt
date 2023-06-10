42b743291496e1dd6caacc0f1a606cd616f1496a
andylamax <andylamax@programmer.net>
2023-06-10 20:38:37 +0300

    Made things on compose

--- input/image/compose/html/src/jsMain/kotlin/symphony/ImageUploader.kt
+++ input/image/compose/html/src/jsMain/kotlin/symphony/ImageUploader.kt
@@ -1,9 +1,23 @@
 package symphony
 
 import androidx.compose.runtime.Composable
+import cinematic.watchAsState
 import epsilon.BrowserBlob
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.DisplayStyle
+import org.jetbrains.compose.web.css.backgroundColor
+import org.jetbrains.compose.web.css.cursor
+import org.jetbrains.compose.web.css.display
+import org.jetbrains.compose.web.css.gridTemplateColumns
+import org.jetbrains.compose.web.css.gridTemplateRows
+import org.jetbrains.compose.web.css.height
+import org.jetbrains.compose.web.css.outline
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.width
+import org.jetbrains.compose.web.dom.Canvas
 import org.jetbrains.compose.web.dom.Div
-import org.jetbrains.compose.web.dom.Text
+import org.jetbrains.compose.web.dom.FileInput
 
 @Composable
 fun ImageUploader(
@@ -13,7 +27,73 @@ fun ImageUploader(
     onSave: ((BrowserBlob) -> Unit)? = null,
     color: String? = null
 ) {
-    Div {
-        Text("Testing sstuff")
+
+    val state = scene.state.watchAsState()
+    val primaryColor = Color(color ?: "gray")
+
+    Div({
+        style {
+            display(DisplayStyle.Flex)
+            outline(width = 2.px, style = "solid", color = primaryColor)
+            width(100.percent)
+            height(100.percent)
+            cursor("pointer")
+        }
+
+        onDrop {
+            it.preventDefault()
+            scene.editFirst(it.dataTransfer?.files)
+        }
+
+        onDragOver { it.preventDefault() }
+
+        onClick {
+            if (state is AwaitingImage) {
+                // TODO: click the input here
+            }
+        }
+
+        onDoubleClick { /* TODO: click the input here */ }
+
+    }) {
+        FileInput {
+            style { display(DisplayStyle.None) }
+            ref {
+                onDispose {}
+            }
+            onChange { scene.editFirst(it.target.files) }
+        }
+
+        Div({
+            style {
+                display(DisplayStyle.Grid)
+                height(100.percent)
+                gridTemplateColumns("1fr")
+                gridTemplateRows("1fr auto")
+            }
+        }) {
+            when (state) {
+                is AwaitingImage -> placeholder?.invoke() ?: ImageUploaderPlaceholder()
+                is EditingImage -> Canvas({
+                    style {
+                        width(100.percent)
+                        height(100.percent)
+                        backgroundColor(primaryColor)
+                        cursor("move")
+                    }
+                }) { }
+
+                is LoadingToEditImage -> TODO()
+                is UploadingImage -> TODO()
+                is ViewingImage -> TODO()
+            }
+        }
+
+        Div({
+            style { width(100.percent) }
+            onClick {
+                console.log("Print do save")
+            }
+        }) { save?.invoke() ?: ImageUploaderSave() }
     }
 }
--- input/image/web/src/main/kotlin/symphony/FileBlobImageUtils.kt
+++ input/image/web/src/main/kotlin/symphony/FileBlobImageUtils.kt
@@ -3,12 +3,19 @@ package symphony
 import epsilon.FileBlob
 import epsilon.fileBlobOf
 import js.core.asList
-import web.file.FileList
+import org.w3c.dom.asList
+import org.w3c.files.FileList as W3CFileList
+import web.file.FileList as WebFileList
 import web.html.Image
 
 fun FileBlob.toImage() = Image().apply { src = path }
 
-fun ImageViewerUploader.editFirst(files: FileList?) {
+fun ImageViewerUploader.editFirst(files: WebFileList?) {
     val file = files?.asList()?.firstOrNull() ?: return
     edit(fileBlobOf(file.unsafeCast<org.w3c.files.File>()))
+}
+
+fun ImageViewerUploader.editFirst(files: W3CFileList?) {
+    val file = files?.asList()?.firstOrNull() ?: return
+    edit(fileBlobOf(file))
 }
