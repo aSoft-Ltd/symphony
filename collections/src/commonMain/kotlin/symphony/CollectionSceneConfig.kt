9e027f904be26054c43522636ace56c46d9a23ac
andylamax <andylamax@programmer.net>
2023-08-04 19:19:18 +0300

    Before auto merging main

--- collections/src/commonMain/kotlin/symphony/CollectionSceneConfig.kt
+++ collections/src/commonMain/kotlin/symphony/CollectionSceneConfig.kt
@@ -2,6 +2,7 @@ package symphony
 
 import keep.Cacheable
 
+@Deprecated("do not use this any more")
 interface CollectionSceneConfig<out T> : Cacheable {
     val actions: SelectorBasedActionsBuilder<@UnsafeVariance T>.() -> Unit
     val columns: ColumnsBuilder<@UnsafeVariance T>.() -> Unit
