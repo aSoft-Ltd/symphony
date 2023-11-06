f9d160762874141eaba4691925d9d736f2671404
andylamax <andylamax@programmer.net>
2023-11-06 18:13:51 +0300

    "Making the console logs user friendly"

---
+++ table/src/commonMain/kotlin/symphony/ITableRendererStringUtils.kt
@@ -0,0 +1,46 @@
+package symphony
+
+import kotlin.math.max
+
+private fun <D> ITable<D>.text(row: Row<D>, col: Column<D>) = when (col) {
+    is Column.Select -> if (selector.isRowSelectedOnCurrentPage(row.number)) "[x]" else "[ ]"
+    is Column.Data -> col.resolve(row)
+    is Column.Action -> actions.of(row.item).joinToString(separator = "|") { it.name }
+}
+
+private fun <D> ITable<D>.text(col: Column<D>) = when (col) {
+    is Column.Data -> col.name
+    is Column.Action -> col.name
+    is Column.Select -> when {
+        selector.isCurrentPageSelectedWholly() -> "[x]"
+        selector.isCurrentPageSelectedPartially() -> "[-]"
+        else -> "[ ]"
+    }
+}
+
+private fun <D> ITable<D>.calculateColSizes(gap: Int): MutableMap<Column<D>, Int> {
+    val colSizes = mutableMapOf<Column<D>, Int>()
+    val visibleColumns = columns.all().filter { it.visibility.isVisible }
+    rows.forEach { row ->
+        visibleColumns.forEach { col ->
+            colSizes[col] = maxOf(colSizes[col] ?: 0, text(row, col).length + gap)
+        }
+    }
+    return colSizes
+}
+
+private fun StringBuilder.appendRow(text: String, size: Int?) {
+    append(text + " ".repeat(max((size ?: 0) - text.length, 0)))
+}
+
+fun <D> ITable<D>.renderToString(gap: Int = 4) = buildString {
+    val colSizes = calculateColSizes(gap)
+    val visibleColumns = columns.all().filter { it.visibility.isVisible }
+    visibleColumns.forEach { appendRow(text(it), colSizes[it]) }
+    appendLine()
+    appendLine()
+    rows.forEach { row ->
+        visibleColumns.forEach { col -> appendRow(text(row, col), colSizes[col]) }
+        appendLine()
+    }
+}
